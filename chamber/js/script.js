5f83322abf8c27e09984e768f4cf1cd83bfcda9a
jodiecouch <couchjodie@gmail.com>
2022-05-26 00:16:12 -0600

    nav using grid. included medium css

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -5,3 +5,9 @@ function toggleMenu(){
 
 const x = document.getElementById('hamburgerBtn')
 x.onclick = toggleMenu;
+
+/*header date*/
+const today = new Date();
+const longDay = new Intl.DateTimeFormat("en-US",{dateStyle:"full"}).format(today);
+const headerDate = document.querySelector(".headerDate");
+headerDate.innerHTML = `<em>${longDay}</em>`;
