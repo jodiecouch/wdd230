60a0033b90bf74948bed626a0078bd248b77cd82
jodiecouch <couchjodie@gmail.com>
2022-10-29 14:22:16 -0600

    lazy loading with intersector

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -38,3 +38,37 @@ if (dow == 1 || dow == 2) {
     toggleBanner();
 }
 
+/*code for lazy loading with intersector */
+
+const imgOptions ={
+    threshold: 1,
+    rootMargin : "0px 0px 100px 0px"
+};
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
