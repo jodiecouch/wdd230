b783e2e436c433b84345255bbb15e4f129cb11f7
jodiecouch <couchjodie@gmail.com>
2022-12-14 14:19:06 -0700

    specialty card and date modified

--- bountiful/js/script.js
+++ bountiful/js/script.js
@@ -2,8 +2,17 @@ function toggleMenu(){
     
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
+    
+    if ( document.querySelector('#drinks-ordered')){
+         document.getElementById("drinks-ordered").classList.toggle("open");
+         }    
 }
 
 const ham = document.getElementById("hamburgerBtn");
 
 ham.onclick = toggleMenu;
+
+/*last updated date*/
+let modifyDate = new Date(document.lastModified).toLocaleString();
+const dateModified = document.querySelector('#last-update');
+dateModified.textContent = `Last Updated: ${modifyDate}`;
